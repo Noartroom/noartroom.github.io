dc1b59c7524dec025ba903559e0f9198695c0b4f
PROJECTS 2024 <96666531+Noartroom@users.noreply.github.com>
2024-06-02 10:32:55 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,8 @@
-/* import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Noartroom.github.io',
-  integrations: [tailwind()]
-}); */
-
-// astro.config.mjs
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-// build config?
-export default defineConfig({
-  // Integrating Tailwind CSS with Astro
-
-});
+  site: 'https://Noartroom.github.io'
+ // integrations: [tailwind()]
+}); 
