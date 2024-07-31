c42bcc9353d1e35a2c1508d9ef62a14b5e8f41ec
Noartroom <noartroom@gmail.com>
2024-07-31 22:08:03 +0200

    latest version blog template and old deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Noartroom.github.io'
- // integrations: [tailwind()]
-}); 
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
+});
