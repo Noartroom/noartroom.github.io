8834d832f2d5c7155c2526e58ba5cc97c31a58a8
Noartroom <noartroom@gmail.com>
2024-08-12 22:10:57 +0200

    try to resolve errors by renaming git

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	/* site: 'https://noartroom.github.io/blog', */
-	site: 'https://noartroom.github.io',
+	site: 'https://noartroom.github.io/',
 	trailingSlash: 'ignore',
 /* 	trailingSlash: 'never',
  */	/* base: '/blog',  */
