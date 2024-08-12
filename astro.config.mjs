2fd0eeaa3f6ea89be34a07940877c5727e14784c
Noartroom <noartroom@gmail.com>
2024-08-12 19:04:15 +0200

    more debug production still not working in chrome

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://noartroom.github.io/blog',
-	/* base: '/blog', */
+	base: '/',
 	integrations: [sitemap()]
 });
