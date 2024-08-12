ac24b1c532e52c873aa681531e8b59c606bcf3d8
Noartroom <noartroom@gmail.com>
2024-08-12 21:44:00 +0200

    resolve all images without errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://noartroom.github.io/blog/',
+	site: 'https://noartroom.github.io/blog',
 /* 	trailingSlash: 'never',
- */	base: '/', 
+ */	base: '/blog', 
 	integrations: [sitemap()]
 });
