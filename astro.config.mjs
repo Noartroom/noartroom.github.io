dc40636e44be312f6ce96ed4264d5569d803d54f
itsnilskerwer <noartroom@gmail.com>
2025-04-15 19:42:22 +0200

    deleted unecessary fonts, files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	experimental: {
+		headingIdCompat: true,
+	  },
 	/* site: 'https://noartroom.github.io/blog', */
 	site: 'https://noartroom.github.io/',
 	/* trailingSlash: 'ignore', */
 /* 	trailingSlash: 'never',
  */	/* base: '/blog',  */
+ 
 	integrations: [sitemap()]
 });
