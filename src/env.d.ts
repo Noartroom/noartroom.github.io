b80092febcaa1f3dd0d04c11d5b7a15935d23fc2
noartroom <noartroom@gmail.com>
2025-10-06 03:13:29 +0200

    Debugging unsuccessful - type error

--- src/env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="astro/client" />
-
-declare module 'playcanvas/build/playcanvas.mjs' {
-  export * from 'playcanvas';
-  export { default } from 'playcanvas'; // probably not needed
-}
